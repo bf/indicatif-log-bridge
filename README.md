14bafa53d40e98e8b39f755d7bec7858e4a7780f
djugei <ddjugei@gmail.com>
2024-08-24 12:36:42 +0200

    document log level issues

--- README.md
+++ README.md
@@ -1,25 +1,27 @@
 # Indicatif Log Bridge
 
-<!-- cargo-sync-readme start -->
+<!-- cargo-rdme start -->
 
-Tired of your log lines and progress bars mixing up? indicatif\_log\_bridge to the rescue!
+Tired of your log lines and progress bars mixing up? indicatif_log_bridge to the rescue!
 
-Simply wrap your favourite logging implementation in [LogWrapper](https://docs.rs/indicatif-log-bridge/*/indicatif-log-bridge/struct.LogWrapper.html)
+Simply wrap your favourite logging implementation in [LogWrapper]
     and those worries are a thing of the past.
 
-Just remember to only use progress bars added to the [MultiProgress](https://docs.rs/indicatif/*/indicatif/struct.MultiProgress.html) you used
+Just remember add each [ProgressBar](indicatif::ProgressBar) to the [MultiProgress] you used
     , otherwise you are back to ghostly halves of progress bars everywhere.
 
-# Example
+## Example
 ```rust
     let logger =
         env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info"))
             .build();
+    let level = logger.filter();
     let multi = MultiProgress::new();
 
     LogWrapper::new(multi.clone(), logger)
         .try_init()
         .unwrap();
+    log::set_max_level(level);
 
     let pg = multi.add(ProgressBar::new(10));
     for i in (0..10) {
@@ -32,4 +34,20 @@ Just remember to only use progress bars added to the [MultiProgress](https://doc
 ```
 The code of this crate is pretty simple, so feel free to check it out.
 
-<!-- cargo-sync-readme end -->
+
+## Known Issues
+### Wrong Global Log Level
+The log framework has a global minimum level, set using [log::set_max_level].
+If that is set to Debug, the trace! macro will not fire at all.
+The [Log] trait does not provide a standartized way of querying the expected level.
+[LogWrapper::try_init] tries hard to find the correct level, but does not always get it right,
+    especially if different levels are specified for different modules or crates,
+        as is often the case with the `env_logger` crate.
+
+#### Workaround
+For `env_logger` specifically you can use `logger.filter()` to query the level
+before constructing and initializing the [LogWrapper] and then passit to [log::set_max_level]
+afterwards.
+If you copy the [example code](#example) you should be fine.
+
+<!-- cargo-rdme end -->
--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 //! Simply wrap your favourite logging implementation in [LogWrapper]
 //!     and those worries are a thing of the past.
 //!
-//! Just remember to only use progress bars added to the [MultiProgress] you used
+//! Just remember add each [ProgressBar](indicatif::ProgressBar) to the [MultiProgress] you used
 //!     , otherwise you are back to ghostly halves of progress bars everywhere.
 //!
 //! # Example
@@ -15,11 +15,13 @@
 //!     let logger =
 //!         env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info"))
 //!             .build();
+//!     let level = logger.filter();
 //!     let multi = MultiProgress::new();
 //!
 //!     LogWrapper::new(multi.clone(), logger)
 //!         .try_init()
 //!         .unwrap();
+//!     log::set_max_level(level);
 //!
 //!     let pg = multi.add(ProgressBar::new(10));
 //!     for i in (0..10) {
@@ -31,15 +33,31 @@
 //!     multi.remove(&pg);
 //! ```
 //! The code of this crate is pretty simple, so feel free to check it out.
+//!
+//!
+//! # Known Issues
+//! ## Wrong Global Log Level
+//! The log framework has a global minimum level, set using [log::set_max_level].
+//! If that is set to Debug, the trace! macro will not fire at all.
+//! The [Log] trait does not provide a standartized way of querying the expected level.
+//! [LogWrapper::try_init] tries hard to find the correct level, but does not always get it right,
+//!     especially if different levels are specified for different modules or crates,
+//!         as is often the case with the `env_logger` crate.
+//!
+//! ### Workaround
+//! For `env_logger` specifically you can use `logger.filter()` to query the level
+//! before constructing and initializing the [LogWrapper] and then passit to [log::set_max_level]
+//! afterwards.
+//! If you copy the [example code](#example) you should be fine.
 
 use indicatif::MultiProgress;
 use log::Log;
 
-/// Wraps a MultiProgress and a Log implementor
-/// calling .suspend on the MultiProgress while writing the log message
+/// Wraps a MultiProgress and a Log implementor,
+/// calling .suspend on the MultiProgress while writing the log message,
 /// thereby preventing progress bars and logs from getting mixed up.
 ///
-/// You simply have to add all the progress bars in use to the MultiProgress in use.
+/// You simply have to add every ProgressBar to the passed MultiProgress.
 pub struct LogWrapper<L: Log> {
     bar: MultiProgress,
     log: L,
@@ -50,11 +68,12 @@ impl<L: Log + 'static> LogWrapper<L> {
         Self { bar, log }
     }
 
-    /// Installs this as the global logger,
+    /// Installs this as the global logger.
     ///
-    /// tries to find the correct argument to set_max_level
+    /// Tries to find the correct argument to log::set_max_level
     /// by reading the logger configuration,
     /// you may want to set it manually though.
+    /// For more details read the [known issues](index.html#wrong-global-log-level).
     pub fn try_init(self) -> Result<(), log::SetLoggerError> {
         use log::LevelFilter::*;
         let levels = [Off, Error, Warn, Info, Debug, Trace];
@@ -79,6 +98,7 @@ impl<L: Log + 'static> LogWrapper<L> {
         self.bar.clone()
     }
 }
+
 impl<L: Log> Log for LogWrapper<L> {
     fn enabled(&self, metadata: &log::Metadata) -> bool {
         self.log.enabled(metadata)
